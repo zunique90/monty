d965b4aabee92abbbfe5d54891db63b966dd0ca8
charles ezebuike <ezebuike.charles0@gmail.com>
2022-12-14 17:55:40 +0000

    tests

---
+++ monty.c
@@ -0,0 +1,45 @@
+#include "monty.h"
+
+/**
+ * main - entry point of the program
+ * @argc: argument count
+ * @argv: argument list
+ * Return: Nothing
+ */
+int main(int argc, char *argv[])
+{
+	char **tokens = NULL;
+	stack_t *head = NULL;
+	char *buffer = NULL;
+	FILE *fp;
+	size_t n;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	fp = fopen(argv[1], "r+");
+	if (fp == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while ((getline(&buffer, &n, fp)) != -1)
+	{
+		line_number++;
+		tokens = tokenize(buffer); /* result is at top of list */
+		if (tokens)
+		{
+			call(tokens, &head);
+			free(tokens);
+		}
+	}
+	free(buffer);
+	free_stack(&head);
+	fclose(fp);
+
+	return (0);
+}
